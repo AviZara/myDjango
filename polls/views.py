bec68de2706058196f9fcc66aaad382dab2b5fc9
holabla <blaholabla@outlook.com>
2018-05-04 17:44:13 +0200

    no message
    Signed-off-by: holabla <blaholabla@outlook.com>

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.utils import timezone
+from .models import Question
+# Create your views here.
 
 def index(request):
-	return HttpResponse(" Hello, world!")
+	latest_question_list=Question.objects.order_by('-pub_date')[:5]
+	output =','.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
 
-# Create your views here.
+def detail(request, question_id):
+	return HttpResponse(" You 're looking at question %s." %question_id)
+
+def results(request,question_id):
+	response =" You are looking at the result of question%s."
+	return HttpResponse (response% question_id)
+def vote(request,question_id):
+	return HttpResponse(" You 're voting on question %s" %question_id)
