b4506d425b60c36e61d0609e0c44c865b4de5aa9
holabla <blaholabla@outlook.com>
2018-05-11 00:14:28 +0300

    no message

--- polls/views.py
+++ polls/views.py
@@ -2,24 +2,33 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone 
+
 
 from .models import Choice, Question
 # Create your views here.
 
 class IndexView(generic.ListView):
 	template_name ='polls/index.html'
-	content_object_name ='latest_question_list'
+	context_object_name ='latest_question_list'
 
 	def get_queryset(self):
 		"""Return the last five published questions."""
 
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 	
 
 class DetailView(generic.DetailView):
 
 	model= Question
 	template_name ="polls/detail.html"
+	def get_queryset(self):
+		"""
+		Excludes any questions taht are not pusblished yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model= Question
